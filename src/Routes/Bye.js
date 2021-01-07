b663b0763476eca3e7e13eaa8483bb8cf0213de0
steven1103 <steven1103@naver.com>
2021-01-07 14:44:43 +0900

    commit before ejecting

---
+++ src/Routes/Bye.js
@@ -0,0 +1,9 @@
+export default function byeRoute() {
+    return (
+        <div>
+            <h1>
+                서버사이드 렌더링을 해보겠습니당 
+            </h1>
+        </div>
+    )
+}
---
+++ src/Routes/Hello.js
@@ -0,0 +1,9 @@
+export default function helloRoute() {
+    return (
+        <div>
+            <h1>
+                Hello 
+            </h1>
+        </div>
+    )
+}
